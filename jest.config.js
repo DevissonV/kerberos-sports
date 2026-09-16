29dbf394eb655b74a2560b1a8f57e53a796340bd
DevissonV <devissonvasquez99@gmail.com>
2026-09-16 17:44:17 -0500

    test(config): excluir artefactos temporales de Jest

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ const config = {
   rootDir: '.',
   setupFiles: ['reflect-metadata'],
   testRegex: '(?<!\\.e2e)\\.spec\\.ts$',
+  testPathIgnorePatterns: ['<rootDir>/resources/temp/'],
   transform: { '^.+\\.(t|j)s$': 'ts-jest' },
   collectCoverageFrom: ['src/**/*.ts', '!src/main.ts'],
   coverageDirectory: 'coverage',
